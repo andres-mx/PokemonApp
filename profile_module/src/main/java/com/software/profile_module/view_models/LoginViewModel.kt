3a0cf41817c05ddb16398f015470b3336b9b25c2
Andy <aleman@outlook.com>
2023-03-09 22:37:11 -0600

    Added LoginViewModelTest at 25%

--- profile_module/src/main/java/com/software/profile_module/view_models/LoginViewModel.kt
+++ profile_module/src/main/java/com/software/profile_module/view_models/LoginViewModel.kt
@@ -8,10 +8,7 @@ import com.software.profile_module_api.domain.EmailValidatorUseCase
 import com.software.profile_module_api.domain.PasswordValidatorUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
